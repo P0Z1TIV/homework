4b2bb879503ad1b9e4235124b7c9a252ad2a2a60
Artem Kislyakov <86775444+P0Z1TIV@users.noreply.github.com>
2023-05-02 13:03:29 +0300

    дз по 6 семинару

---
+++ дз по 6 семинару/дз1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] FillArray (int length)
+{
+    int[] result = new int[length];
+    Console.WriteLine("Введите число элементов массива ");
+    length = int.Parse(Console.ReadLine());
+    for(int i = 0; i < length; i++)
+        {
+        Console.WriteLine($"Введите {i+1} число массива ");
+        result[i] = int.Parse(Console.ReadLine());
+        }
+    return result;
+}
+int GetCount (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+int[] array = FillArray (10);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", GetCount(array)));
---
+++ дз по 6 семинару/дз2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите координату k1 первой точки ");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату b1 первой точки ");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату k2 второй точки ");
+double k2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату b2 первой точки ");
+double b2 = int.Parse(Console.ReadLine());
+double X = (b2 - b1) / (k2 - k1);
+double Y = (k1 * X + b1) - (k2 * X + b2);
+if((k1 * X + b1) == (k2 * X + b2))
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else {
+   
+    Console.WriteLine($"Точка пеересечения {X}, {Y}");
+}
